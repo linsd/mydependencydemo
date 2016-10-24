47f251bc97d719c900f2324dba727221c11bcf6e
linsd <515690961@qq.com>
2016-10-24 17:39:48 +0800

    init project

---
+++ src/main/java/com/tasxxz/mydependencydemo/hello/Hello.java
@@ -0,0 +1,11 @@
+package com.tasxxz.mydependencydemo.hello;
+
+/**
+ * Created by linsd on 2016/10/24.
+ */
+public class Hello {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
